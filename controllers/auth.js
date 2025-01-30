6b8422dfe9c6e85876a3deb74088a1e352c7f2bb
Zed <skhull.coding@proton.me>
2025-01-31 00:05:54 +0530

    auth setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,36 @@
-// if auth dependes on any other files or services
-// add them here
+import jwt from "jsonwebtoken";
+import _env from "../constants/env.js";
+
+const act = _env.auth.jwt.access;
+const rft = _env.auth.jwt.refresh;
+
+const generateAccessToken = (data) => {
+  const accessToken = jwt.sign(data, act.secret, { expiresIn: act.expiry });
+
+  return accessToken;
+};
+
+const generateRefreshToken = ({ key, value }) => {
+  const data = {};
+  data[key] = value;
+  const refreshToken = jwt.sign(data, rft.secret, { expiresIn: rft.expiry });
+
+  return refreshToken;
+};
+
+const verifyAccessToken = (accessToken) => {
+  const data = jwt.verify(accessToken, act.secret);
+  return data;
+};
+
+const verifyRefreshToken = (refreshToken) => {
+  const data = jwt.verify(refreshToken, rft.secret);
+  return data;
+};
+
+const jwtAuth = {
+  generate: { access: generateAccessToken, refresh: generateRefreshToken },
+  verify: { access: verifyAccessToken, refresh: verifyRefreshToken },
+};
+
+export default jwtAuth;
